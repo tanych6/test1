6f25f5692b05fab4fadbac8848c75fb0a982fd7c
[tyc] <470430494@qq.com>
2019-01-16 12:00:50 +0800

    autopep8

--- test_py.py
+++ test_py.py
@@ -1,22 +1,23 @@
-# -*- coding: UTF-8 -*-
+# -*- codJng: UTF-8 -*-
 while True:
- I = input("pls input the lirun:")
- I=int(I)
- if I <= 10:
-  a = I * 0.01
-  print (a)
- elif I <= 20 and I > 10:
-  b =0.25 + I * 0.075
-  print (b)
- elif I <= 40 and I > 20:
-  c = 0.75 + I * 0.05
-  print (c)
- elif I <= 60 and I > 40:
-  d = 0.95 + I * 0.03
-  print (d)
- elif I <= 60 and I > 100:
-  e = 2 + I * 0.015
-  print (e)
- else:
-  f = 2.95 + I * 0.01
-  print (f)
+    J = input("pls input the lirun:")
+    J = int(J)
+    if J <= 10:
+        a = J * 0.01
+        print(a)
+    elif J <= 20 and J > 10:
+        b = 0.25 + J * 0.075
+        print(b)
+    elif J <= 40 and J > 20:
+        c = 0.75 + J * 0.05
+        print(c)
+    elif J <= 60 and J > 40:
+        d = 0.95 + J * 0.03
+        print(d)
+    elif J <= 60 and J > 100:
+        e = 2 + J * 0.015
+        print(e)
+    else:
+        f = 2.95 + J * 0.01
+        print(f)
+    print("OK")
